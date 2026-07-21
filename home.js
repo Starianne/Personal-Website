7693a4cf12d7d39a951533c43e2aaa71714c2603
[Annelies S] <118388665+Starianne@users.noreply.github.com>
2026-07-21 18:18:41 +0100

    Created basic homescreen template

---
+++ home.js
@@ -0,0 +1,7 @@
+const aboutMe = document.getElementById('aboutMe')
+const projects = document.getElementById('projects')
+const todo = document.getElementById('todo')
+const blog = document.getElementById('blog')
+var menuItems = [aboutMe, projects, todo, blog]
+
+//Ok wait maybe Ill use it later
